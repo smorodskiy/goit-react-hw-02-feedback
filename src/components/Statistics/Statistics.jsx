f2a903aba17220c4d91b06302187163b17ec1f43
Igor Smorodsky <ingerard@gmail.com>
2022-10-30 00:32:28 +0300

    Stat styled

--- src/components/Statistics/Statistics.jsx
+++ src/components/Statistics/Statistics.jsx
@@ -2,18 +2,29 @@ import { Component } from 'react';
 
 // Check types of props
 import PropTypes from 'prop-types';
+import { Badge, StatItem, StatList } from './Statistics.styled';
 
 class Statistics extends Component {
   render() {
     const { good, neutral, bad, total, positivePercentage } = this.props;
     return (
-      <ul>
-        <li>Good: {good}</li>
-        <li>Neutral: {neutral}</li>
-        <li>Bad: {bad}</li>
-        <li>Total: {total}</li>
-        <li>Positive feedback: {positivePercentage}%</li>
-      </ul>
+      <StatList>
+        <StatItem>
+          Good: <Badge>{good}</Badge>
+        </StatItem>
+        <StatItem>
+          Neutral: <Badge>{neutral}</Badge>
+        </StatItem>
+        <StatItem>
+          Bad: <Badge>{bad}</Badge>
+        </StatItem>
+        <StatItem>
+          Total: <Badge>{total}</Badge>
+        </StatItem>
+        <StatItem>
+          Positive feedback: <Badge>{positivePercentage}%</Badge>
+        </StatItem>
+      </StatList>
     );
   }
 }
